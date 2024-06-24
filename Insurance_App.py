fc6ec75ddaebe43bb3405e6d3a2b36625fb57ee5
phoenix-prabeena <prabeena.pradhan@hotmail.com>
2024-06-24 23:17:59 +0530

    Insurance_App.py

--- Insurance_App.py
+++ Insurance_App.py
@@ -2,9 +2,6 @@
 import streamlit as st
 import pickle
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 lr1 = pickle.load(open('/home/misfit/Downloads/Python course/Projects/lr1.pkl','rb'))
 dtr1 = pickle.load(open('/home/misfit/Downloads/Python course/Projects/dtr1.pkl','rb'))
